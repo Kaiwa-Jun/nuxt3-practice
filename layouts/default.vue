ebceec02d4880d4eaf31a31df8aeffa5e9bffb94
Kaiwa-Jun <kj.11235813213455@gmail.com>
2024-03-10 22:31:38 +0900

    ルーティング／レイアウト

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <h1>ヘッダー変更サンプル</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>TOP</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>TOP</li>
+    </ul>
+  </nav>
+  <section>
+    <p>
+      <!-- <NuxtLink to="/member/memberList"> -->
+      <NuxtLink v-bind:to="{ name: 'member-memberList' }">
+        会員管理はこちら
+      </NuxtLink>
+    </p>
+  </section>
+</template>
+
+<script></script>
